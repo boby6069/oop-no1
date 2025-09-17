1c8eaf2f6169082e73ea2ee99c5de1e1527f5dd3
USER <USER@DESKTOP-M94GHB0>
2025-09-17 18:28:23 +0800

    Add project files.

---
+++ oop no1/Form1.Designer.vb
@@ -0,0 +1,268 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        txtdisplay = New TextBox()
+        bt1 = New Button()
+        bt2 = New Button()
+        bt3 = New Button()
+        bt4 = New Button()
+        bt5 = New Button()
+        bt6 = New Button()
+        bt7 = New Button()
+        bt8 = New Button()
+        bt9 = New Button()
+        bt0 = New Button()
+        btc = New Button()
+        bta = New Button()
+        bts = New Button()
+        btp = New Button()
+        btd = New Button()
+        btt = New Button()
+        bte = New Button()
+        mali = New Button()
+        SuspendLayout()
+        ' 
+        ' txtdisplay
+        ' 
+        txtdisplay.Location = New Point(308, 71)
+        txtdisplay.Name = "txtdisplay"
+        txtdisplay.ReadOnly = True
+        txtdisplay.Size = New Size(221, 23)
+        txtdisplay.TabIndex = 0
+        txtdisplay.TextAlign = HorizontalAlignment.Right
+        ' 
+        ' bt1
+        ' 
+        bt1.Location = New Point(308, 110)
+        bt1.Name = "bt1"
+        bt1.Size = New Size(49, 50)
+        bt1.TabIndex = 1
+        bt1.Text = "1"
+        bt1.UseVisualStyleBackColor = True
+        ' 
+        ' bt2
+        ' 
+        bt2.Location = New Point(366, 110)
+        bt2.Name = "bt2"
+        bt2.Size = New Size(49, 50)
+        bt2.TabIndex = 2
+        bt2.Text = "2"
+        bt2.UseVisualStyleBackColor = True
+        ' 
+        ' bt3
+        ' 
+        bt3.Location = New Point(423, 110)
+        bt3.Name = "bt3"
+        bt3.Size = New Size(49, 50)
+        bt3.TabIndex = 3
+        bt3.Text = "3"
+        bt3.UseVisualStyleBackColor = True
+        ' 
+        ' bt4
+        ' 
+        bt4.Location = New Point(308, 163)
+        bt4.Name = "bt4"
+        bt4.Size = New Size(49, 50)
+        bt4.TabIndex = 4
+        bt4.Text = "4"
+        bt4.UseVisualStyleBackColor = True
+        ' 
+        ' bt5
+        ' 
+        bt5.Location = New Point(366, 163)
+        bt5.Name = "bt5"
+        bt5.Size = New Size(49, 50)
+        bt5.TabIndex = 5
+        bt5.Text = "5"
+        bt5.UseVisualStyleBackColor = True
+        ' 
+        ' bt6
+        ' 
+        bt6.Location = New Point(423, 163)
+        bt6.Name = "bt6"
+        bt6.Size = New Size(49, 50)
+        bt6.TabIndex = 6
+        bt6.Text = "6"
+        bt6.UseVisualStyleBackColor = True
+        ' 
+        ' bt7
+        ' 
+        bt7.Location = New Point(308, 216)
+        bt7.Name = "bt7"
+        bt7.Size = New Size(49, 50)
+        bt7.TabIndex = 7
+        bt7.Text = "7"
+        bt7.UseVisualStyleBackColor = True
+        ' 
+        ' bt8
+        ' 
+        bt8.Location = New Point(365, 216)
+        bt8.Name = "bt8"
+        bt8.Size = New Size(49, 50)
+        bt8.TabIndex = 8
+        bt8.Text = "8"
+        bt8.UseVisualStyleBackColor = True
+        ' 
+        ' bt9
+        ' 
+        bt9.Location = New Point(423, 216)
+        bt9.Name = "bt9"
+        bt9.Size = New Size(49, 50)
+        bt9.TabIndex = 9
+        bt9.Text = "9"
+        bt9.UseVisualStyleBackColor = True
+        ' 
+        ' bt0
+        ' 
+        bt0.Location = New Point(307, 269)
+        bt0.Name = "bt0"
+        bt0.Size = New Size(50, 50)
+        bt0.TabIndex = 10
+        bt0.Text = "0"
+        bt0.UseVisualStyleBackColor = True
+        ' 
+        ' btc
+        ' 
+        btc.Location = New Point(480, 110)
+        btc.Name = "btc"
+        btc.Size = New Size(49, 50)
+        btc.TabIndex = 11
+        btc.Text = "C"
+        btc.UseVisualStyleBackColor = True
+        ' 
+        ' bta
+        ' 
+        bta.Location = New Point(480, 163)
+        bta.Name = "bta"
+        bta.Size = New Size(49, 50)
+        bta.TabIndex = 12
+        bta.Text = "+"
+        bta.UseVisualStyleBackColor = True
+        ' 
+        ' bts
+        ' 
+        bts.Location = New Point(480, 216)
+        bts.Name = "bts"
+        bts.Size = New Size(49, 50)
+        bts.TabIndex = 13
+        bts.Text = "-"
+        bts.UseVisualStyleBackColor = True
+        ' 
+        ' btp
+        ' 
+        btp.Location = New Point(365, 269)
+        btp.Name = "btp"
+        btp.Size = New Size(49, 50)
+        btp.TabIndex = 14
+        btp.Text = "."
+        btp.UseVisualStyleBackColor = True
+        ' 
+        ' btd
+        ' 
+        btd.Location = New Point(480, 269)
+        btd.Name = "btd"
+        btd.Size = New Size(49, 50)
+        btd.TabIndex = 15
+        btd.Text = "/"
+        btd.UseVisualStyleBackColor = True
+        ' 
+        ' btt
+        ' 
+        btt.Location = New Point(424, 272)
+        btt.Name = "btt"
+        btt.Size = New Size(50, 50)
+        btt.TabIndex = 16
+        btt.Text = "*"
+        btt.UseVisualStyleBackColor = True
+        ' 
+        ' bte
+        ' 
+        bte.Location = New Point(308, 328)
+        bte.Name = "bte"
+        bte.Size = New Size(164, 50)
+        bte.TabIndex = 17
+        bte.Text = "="
+        bte.UseVisualStyleBackColor = True
+        ' 
+        ' mali
+        ' 
+        mali.Location = New Point(478, 325)
+        mali.Name = "mali"
+        mali.Size = New Size(50, 50)
+        mali.TabIndex = 18
+        mali.Text = "mali?"
+        mali.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(819, 477)
+        Controls.Add(mali)
+        Controls.Add(bte)
+        Controls.Add(btt)
+        Controls.Add(btd)
+        Controls.Add(btp)
+        Controls.Add(bts)
+        Controls.Add(bta)
+        Controls.Add(btc)
+        Controls.Add(bt0)
+        Controls.Add(bt9)
+        Controls.Add(bt8)
+        Controls.Add(bt7)
+        Controls.Add(bt6)
+        Controls.Add(bt5)
+        Controls.Add(bt4)
+        Controls.Add(bt3)
+        Controls.Add(bt2)
+        Controls.Add(bt1)
+        Controls.Add(txtdisplay)
+        KeyPreview = True
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents txtdisplay As TextBox
+    Friend WithEvents bt1 As Button
+    Friend WithEvents bt2 As Button
+    Friend WithEvents bt3 As Button
+    Friend WithEvents bt4 As Button
+    Friend WithEvents bt5 As Button
+    Friend WithEvents bt6 As Button
+    Friend WithEvents bt7 As Button
+    Friend WithEvents bt8 As Button
+    Friend WithEvents bt9 As Button
+    Friend WithEvents bt0 As Button
+    Friend WithEvents btc As Button
+    Friend WithEvents bta As Button
+    Friend WithEvents bts As Button
+    Friend WithEvents btp As Button
+    Friend WithEvents btd As Button
+    Friend WithEvents btt As Button
+    Friend WithEvents bte As Button
+    Friend WithEvents mali As Button
+
+End Class
---
+++ oop no1/Form1.vb
@@ -0,0 +1,156 @@
+﻿Public Class Form1
+    Private isResult As Boolean = False
+    Private justPressedOperator As Boolean = False
+    Private lastResult As Double = 0
+    Private operation As String
+
+    Private Sub btp_Click(sender As Object, e As EventArgs) Handles btp.Click
+        If Not txtdisplay.Text.Contains(".") Then
+            If txtdisplay.Text = "" Then
+                txtdisplay.Text = "0."
+            Else
+                txtdisplay.Text &= "."
+            End If
+        End If
+    End Sub
+
+    ' === Operator Buttons (+ - × ÷) ===
+    Private Sub btnOperator_Click(sender As Object, e As EventArgs) _
+        Handles bta.Click, bts.Click, btt.Click, btd.Click
+
+        Dim btn As Button = CType(sender, Button)
+        Dim op As String = btn.Text
+
+        ' Case 1: Start with negative number
+        If txtdisplay.Text = "" AndAlso op = "-" Then
+            txtdisplay.Text = "-"
+            isResult = False
+            justPressedOperator = False
+            Return
+        End If
+
+        ' Case 2: Allow negative numbers after operator
+        If txtdisplay.Text.EndsWith(" ") AndAlso op = "-" Then
+            txtdisplay.Text &= "-"
+            isResult = False
+            justPressedOperator = False
+            Return
+        End If
+
+        ' Case 3: If last was a result, continue from it
+        If isResult Then
+            txtdisplay.Text = lastResult.ToString() & " " & op & " "
+            isResult = False
+            justPressedOperator = True
+            Return
+        End If
+
+        ' Case 4: Replace or append operator
+        If txtdisplay.Text.EndsWith(" ") Then
+            txtdisplay.Text = txtdisplay.Text.Substring(0, txtdisplay.Text.Length - 3) & " " & op & " "
+        ElseIf txtdisplay.Text <> "" AndAlso txtdisplay.Text <> "-" Then
+            txtdisplay.Text &= " " & op & " "
+        End If
+
+        justPressedOperator = True
+    End Sub
+
+    ' === Clear Button (C) ===
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btc.Click
+        txtdisplay.Clear()
+        lastResult = 0
+        operation = ""
+        isResult = False
+        justPressedOperator = False
+    End Sub
+
+    ' === Equal Button 😊) ===
+    Private Sub bte_Click(sender As Object, e As EventArgs) Handles bte.Click
+        Dim expr As String = txtdisplay.Text.Trim()
+        If expr = "" Then Return
+
+        expr = expr.Replace("×", "*").Replace("÷", "/")
+
+        Try
+            Dim dt As New DataTable()
+            Dim result = dt.Compute(expr, Nothing)
+            lastResult = Convert.ToDouble(result)
+
+            txtdisplay.Text = lastResult.ToString()
+            isResult = True
+            justPressedOperator = False
+        Catch ex As Exception
+            MessageBox.Show("You cant put operator on hold")
+        End Try
+    End Sub
+
+    ' === Backspace Button (⌫) ===
+    Private Sub Button1_Click_2(sender As Object, e As EventArgs) Handles mali.Click
+        If isResult Then Exit Sub
+        If txtdisplay.Text.Length > 0 Then
+            txtdisplay.Text = txtdisplay.Text.Substring(0, txtdisplay.Text.Length - 1)
+        End If
+    End Sub
+
+    Private Sub Form1_KeyPress(sender As Object, e As KeyPressEventArgs) Handles Me.KeyPress
+        Select Case e.KeyChar
+        ' === Equal / Enter ===
+            Case ChrW(Keys.Enter), "="c
+                bte.PerformClick()
+                e.Handled = True
+
+        ' === Numbers 0–9 ===
+            Case "0"c To "9"c
+                Dim btnName As String = "bt" & e.KeyChar
+                Dim btn As Button = Me.Controls.Find(btnName, True).FirstOrDefault()
+                If btn IsNot Nothing Then btn.PerformClick()
+                e.Handled = True
+
+        ' === Operators (+ - × ÷) ===
+            Case "+"c, "-"c, "*"c, "/"c
+                Dim btnName As String = ""
+                Select Case e.KeyChar
+                    Case "+"c : btnName = "bta"
+                    Case "-"c : btnName = "bts"
+                    Case "*"c : btnName = "btt"
+                    Case "/"c : btnName = "btd"
+                End Select
+                Dim btn As Button = Me.Controls.Find(btnName, True).FirstOrDefault()
+                If btn IsNot Nothing Then btn.PerformClick()
+                e.Handled = True
+
+        ' === Decimal Point ===
+            Case "."c
+                btp.PerformClick()
+                e.Handled = True
+
+        ' === Backspace (⌫) ===
+            Case ChrW(Keys.Back)
+                mali.PerformClick()
+                e.Handled = True
+
+        ' === Clear (C or c) ===
+            Case "c"c, "C"c
+                btc.PerformClick()
+                e.Handled = True
+        End Select
+    End Sub
+    ' === Number Buttons (0–9) ===
+    Private Sub NumberButton_Click(sender As Object, e As EventArgs) Handles bt0.Click, bt1.Click, bt2.Click, bt3.Click, bt4.Click, bt5.Click, bt6.Click, bt7.Click, bt8.Click, bt9.Click
+        Dim btn As Button = CType(sender, Button)
+
+        If isResult Then
+            ' After = and typing a number → new calculation
+            txtdisplay.Text = btn.Text
+            isResult = False
+            justPressedOperator = False
+        ElseIf justPressedOperator Then
+            ' After operator → append number
+            txtdisplay.Text &= btn.Text
+            justPressedOperator = False
+        Else
+            ' Normal typing
+            txtdisplay.Text &= btn.Text
+        End If
+    End Sub
+End Class
